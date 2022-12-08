5a506a778bac1187f9633a1173cd6bedaf8b3c06
hasanmd91 <hasanmd1991@hotmail.com>
2022-12-08 15:08:34 +0200

    build: pages

--- Hasan_Md_flower_project/index.js
+++ Hasan_Md_flower_project/index.js
@@ -1,3 +1,33 @@
-import express from "express";
-
+const express = require("express");
+const path = require("path");
 const app = express();
+
+const { port, host } = require("./serverConfig.json");
+
+const menuPath = path.join(__dirname, "menu.html");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "pages"));
+
+// rendering home page
+app.get("/", (req, res) => {
+  res.sendFile(menuPath);
+});
+
+// rendering all flower page
+app.get("/all", (req, res) => {
+  res.render("allFlower");
+});
+
+// rendering one flower page
+app.get("/getflower", (req, res) => {
+  res.render("oneFlower");
+});
+
+// rendering add flower page
+
+app.get("/inputform", (req, res) => {
+  res.render("form");
+});
+
+app.listen(port, host, () => console.log("server is listening "));
